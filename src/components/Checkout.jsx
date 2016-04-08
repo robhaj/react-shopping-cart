3e782ca04b2e1730733c31dc03e9a830df94b89e
Robert Hajek <hajek.rob@gmail.com>
2016-04-07 21:51:35 -0600

    scaffold components

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Checkout = () => {
+  return (
+
+  )
+}
+
+export default Checkout
---
+++ src/components/Item.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Item = () => {
+  return (
+
+  )
+}
+
+export default Item
